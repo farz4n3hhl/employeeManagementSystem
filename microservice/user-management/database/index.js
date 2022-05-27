e26e51f29ab4f89c2104292836282f42a23c4244
Maedeh <maede.nadehi@yahoo.com>
2022-05-27 20:48:18 +0430

    user management

--- microservice/user-management/database/index.js
+++ microservice/user-management/database/index.js
@@ -1,5 +1,16 @@
-const { redisOptions } = require('../config')
 const redis = require('redis')
+
+// redis config
+const redisOptions = {
+    host: '127.0.0.1',
+    port: 6379,
+    maxRetriesPerRequest: 4,  // only retry failed requests 4 times
+    enableOfflineQueue: true, // queue offline requests
+    retryStrategy: function(times) {
+      return 2000 // reconnect after 2 seconds
+    },
+}
+
 let dataBase = redis.createClient(redisOptions)
 
 const connectToDB = function(index) {
--- microservice/user-management/utils/errorHandler.js
+++
@@ -1,7 +0,0 @@
-exports.errorHandler = function(err, event) {
-    const errorObject = {
-        at: event,
-        reason: err
-    }
-    console.error(errorObject);
-}
